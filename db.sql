eedd34e68e6f2df24fe55820e2e9994628637e04
Rémy Schumm <dev@schumm.ch>
2021-11-18 16:54:07 +0100

    echte Daten

--- db.sql
+++ db.sql
@@ -1,77 +1,24 @@
-create table radar (
-    label text primary key; 
-    quadrant int, 
-    ring int, 
-    moved int, 
-    year int    
-)
-
-
-
-
-INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('MariaDB', 3, 2, -1, 2021);
-
-INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('PostgreSQL', 3, 0, 1, 2021);
-INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('Neo4J', 3, 1, 0, 2021);
-INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('MS SQL', 3, 3, -1, 2021);
-INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('DDD', 2, 1, 1, 2021);
-INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('TDD', 2, 1, 1, 2021);
-
-INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('GrpaphQL', 1, 0, 0, 2021);
-INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('REST', 1, 0, 0, 2021);
-
-
-
-
-select json_build_object(
-    'entries', json_agg( 
-                    json_build_object(
-                        'label', label,
-						'quadrant', quadrant, 
-						'ring', ring,
-						'moved', moved, 
-                        'active', 'true'
-                    )
-                )
-)
-from radar; 
-
--- mit Enums
-
-
-
-
 create type radarval as enum 
 (
     'platforms', 'patterns', 'methods', 'data',
     'adopt', 'trial', 'eval', 'hold'
-)
+); 
 
 
-create table radar_e (
-    label text primary key; 
+create table radar (
+    label text primary key, 
     quadrant radarval, 
     ring radarval, 
     moved int, 
     year int    
-)
-
-
-INSERT INTO public.radar_e(label, quadrant, ring, moved, year) VALUES ('MariaDB', 'data', 'adopt', -1, 2021);
-INSERT INTO public.radar_e(label, quadrant, ring, moved, year) VALUES ('PostgreSQL', 'data', 'eval', 1, 2021);
-INSERT INTO public.radar_e(label, quadrant, ring, moved, year) VALUES ('Neo4J', 'data', 'eval', 0, 2021);
-INSERT INTO public.radar_e(label, quadrant, ring, moved, year) VALUES ('MS SQL', 'data', 'hold', -1, 2021);
-INSERT INTO public.radar_e(label, quadrant, ring, moved, year) VALUES ('DDD', 'methods', 'adopt', 1, 2021);
-INSERT INTO public.radar_e(label, quadrant, ring, moved, year) VALUES ('TDD', 'methods', 'adopt', 1, 2021);
-INSERT INTO public.radar_e(label, quadrant, ring, moved, year) VALUES ('GrpaphQL', 'patterns', 'adopt', 0, 2021);
-INSERT INTO public.radar_e(label, quadrant, ring, moved, year) VALUES ('REST', 'patterns', 'adopt', 0, 2021);
+); 
 
 
 create table radarvalmap (
     val radarval, 
     number int, 
     unique (val, number)
-)
+); 
 
 
 insert into radarvalmap values ('platforms', 0); 
@@ -87,15 +34,11 @@ insert into radarvalmap values ('hold', 3);
 
 
 
-select *
-from radar_e 
-join radarvalmap valq on (radar_e.quadrant = valq.val) 
-join radarvalmap valr on (radar_e.ring = valr.val)
-where label = 'MariaDB'
 
 
 
-select json_build_object(
+
+select year, json_build_object(
     'entries', json_agg( 
                     json_build_object(
                         'label', label,
@@ -106,6 +49,52 @@ select json_build_object(
                     )
                 )
 )
-from radar_e 
-join radarvalmap valq on (radar_e.quadrant = valq.val) 
-join radarvalmap valr on (radar_e.ring = valr.val)
+from radar 
+join radarvalmap valq on (radar.quadrant = valq.val) 
+join radarvalmap valr on (radar.ring = valr.val)
+group by year
+
+
+
+
+select *
+from radar 
+join radarvalmap valq on (radar.quadrant = valq.val) 
+join radarvalmap valr on (radar.ring = valr.val)
+where label = 'MariaDB'; 
+
+
+
+-- DATA 
+
+
+INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('MariaDB', 'data', 'adopt', 0, 2021);
+INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('PostgreSQL', 'data', 'eval', 1, 2021);
+INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('Neo4J', 'data', 'eval', 0, 2021);
+INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('MongoDB', 'data', 'hold', -1, 2021);
+
+
+INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('DDD', 'methods', 'adopt', 1, 2021);
+INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('TDD', 'methods', 'trial', +1, 2021);
+INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('Scrum', 'methods', 'adopt', 0, 2021);
+
+
+
+INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('GrpaphQL', 'patterns', 'adopt', 0, 2021);
+INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('REST', 'patterns', 'adopt', 0, 2021);
+INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('SharedDB', 'patterns', 'trial', +1, 2021);
+INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('Strangler', 'patterns', 'adopt', +1, 2021);
+INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('Citadelle', 'patterns', 'adopt', +1, 2021);
+
+
+
+INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('Java', 'platforms', 'adopt', 1, 2021);
+INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('Spring', 'platforms', 'adopt', 0, 2021);
+INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('Angular', 'platforms', 'adopt', 0, 2021);
+INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('ReactJS', 'platforms', 'trial', +1, 2021);
+INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('PHP', 'platforms', 'adopt', -1, 2021);
+INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('chart.js', 'platforms', 'trial', +1, 2021);
+INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('Keycloak', 'platforms', 'eval', +1, 2021);
+INSERT INTO public.radar(label, quadrant, ring, moved, year) VALUES ('OpenShift', 'platforms', 'adopt', 0, 2021);
+
+
