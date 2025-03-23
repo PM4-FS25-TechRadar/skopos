dde00a648a369c9f3f3e386411dd867678358e35
Dean Meier <dean.meier@bluewin.ch>
2025-03-23 18:52:44 +0100

    feat: add radarentry entity and rest endpoints

---
+++ src/main/java/ch/sobrado/radar/RadarEntry.java
@@ -0,0 +1,16 @@
+package ch.sobrado.radar;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "radar_entry")
+public class RadarEntry extends PanacheEntity {
+    public String label;
+    public String quadrant;
+    public String ring;
+    public int moved;
+    public int year;
+}
