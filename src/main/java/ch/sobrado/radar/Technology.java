c9cdea2bdb286abc524c17bcb3e65a5bf82c96a5
Dean Meier <dean.meier@bluewin.ch>
2025-03-28 10:16:18 +0100

    feat: add technology and version entities with flyway migration

---
+++ src/main/java/ch/sobrado/radar/Technology.java
@@ -0,0 +1,20 @@
+package ch.sobrado.radar;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "technologies")
+public class Technology extends PanacheEntity {
+
+    @Column(nullable = false, unique = true)
+    public String name;
+
+    public String description;
+
+    @OneToMany(mappedBy = "technology", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    public List<Version> versions;
+}
