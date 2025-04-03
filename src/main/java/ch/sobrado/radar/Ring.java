2f0c9432f8c88f4cef8ad0ca8b6514855d0971cf
enzlearo <enzlearo@students.zhaw.ch>
2025-04-03 16:11:58 +0200

    feat: add ring entity

--- src/main/java/ch/sobrado/radar/Ring.java
+++ src/main/java/ch/sobrado/radar/Ring.java
@@ -1,8 +1,23 @@
 package ch.sobrado.radar;
 
-public enum Ring {
-    adopt,
-    trial,
-    eval,
-    hold
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(
+        name = "rings",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"name", "radar_id"})
+        }
+)
+public class Ring extends PanacheEntity {
+
+    @Column(nullable = false)
+    public String name;
+
+    @ManyToOne
+    @JoinColumn(name = "radar_id", nullable = false)
+    @JsonBackReference
+    public Radar radar;
 }
