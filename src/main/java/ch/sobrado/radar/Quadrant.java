ac9ee3a4d3f7441265bf5305c7c490cef0a684da
enzlearo <enzlearo@students.zhaw.ch>
2025-04-03 16:12:18 +0200

    feat: add quadrant entity

--- src/main/java/ch/sobrado/radar/Quadrant.java
+++ src/main/java/ch/sobrado/radar/Quadrant.java
@@ -1,8 +1,23 @@
 package ch.sobrado.radar;
 
-public enum Quadrant {
-    patterns,
-    methods,
-    platforms,
-    data
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(
+        name = "quadrants",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"name", "radar_id"})
+        }
+)
+public class Quadrant extends PanacheEntity {
+
+    @Column(nullable = false)
+    public String name;
+
+    @ManyToOne
+    @JoinColumn(name = "radar_id", nullable = false)
+    @JsonBackReference
+    public Radar radar;
 }
