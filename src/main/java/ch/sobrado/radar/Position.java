f803b1cd8289104dda733c02addac3be2c85b75a
enzlearo <enzlearo@students.zhaw.ch>
2025-04-06 18:45:48 +0200

    feat: implement Position enum with order values

--- src/main/java/ch/sobrado/radar/Position.java
+++ src/main/java/ch/sobrado/radar/Position.java
@@ -1,4 +1,18 @@
 package ch.sobrado.radar;
 
 public enum Position {
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4);
+
+    private final int order;
+
+    Position(int order) {
+        this.order = order;
+    }
+
+    public int getOrder() {
+        return order;
+    }
 }
