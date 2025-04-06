d452ba0f195bb1924243068836b96dab4ed10bcc
enzlearo <enzlearo@students.zhaw.ch>
2025-04-06 21:28:05 +0200

    chore: cleanup code and remove unused JSON annotations

--- src/main/java/ch/sobrado/radar/RadarEntry.java
+++ src/main/java/ch/sobrado/radar/RadarEntry.java
@@ -3,7 +3,6 @@ package ch.sobrado.radar;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.*;
 
@@ -13,18 +12,15 @@ public class RadarEntry extends PanacheEntity {
 
     public String label;
 
-    @ManyToOne 
-    @JsonIgnore
+    @ManyToOne
     @JoinColumn(name = "radar_id")
     public Radar radar;
 
     @ManyToOne
-    @JsonBackReference
     @JoinColumn(name = "quadrant_id")
     public Quadrant quadrant;
 
     @ManyToOne
-    @JsonBackReference
     @JoinColumn(name = "ring_id")
     public Ring ring;
 
