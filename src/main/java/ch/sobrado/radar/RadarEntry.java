442b50680bfaf723de373c692e8115c6e938180d
enzlearo <enzlearo@students.zhaw.ch>
2025-04-06 18:51:53 +0200

    feat: add JSON annotations for radar relationship management

--- src/main/java/ch/sobrado/radar/RadarEntry.java
+++ src/main/java/ch/sobrado/radar/RadarEntry.java
@@ -2,6 +2,8 @@ package ch.sobrado.radar;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.*;
 
@@ -11,11 +13,18 @@ public class RadarEntry extends PanacheEntity {
 
     public String label;
 
+    @ManyToOne 
+    @JsonIgnore
+    @JoinColumn(name = "radar_id")
+    public Radar radar;
+
     @ManyToOne
+    @JsonBackReference
     @JoinColumn(name = "quadrant_id")
     public Quadrant quadrant;
 
     @ManyToOne
+    @JsonBackReference
     @JoinColumn(name = "ring_id")
     public Ring ring;
 
