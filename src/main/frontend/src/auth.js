458f49d1b5d7b2ac1e9f0d260909e700de03c261
lukasblau <54887264+lukasblau@users.noreply.github.com>
2025-04-21 20:28:58 +0200

    ci: fixed keycloak frontend realm

--- src/main/frontend/src/auth.js
+++ src/main/frontend/src/auth.js
@@ -2,7 +2,7 @@ import Keycloak from 'keycloak-js'
 
 const keycloak = new Keycloak({
     url: 'https://kc-skopos.pm4.init-lab.ch/',
-    realm: 'quarkus',
+    realm: 'master',
     clientId: 'frontend-app',
 })
 
